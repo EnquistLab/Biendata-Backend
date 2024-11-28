897aee505f9bdc0e531e0ff7135a1a5c0ec40b95
Rethvick <furyassassinofficial@gmail.com>
2024-11-27 17:25:36 -0800

    Added New Routers

--- server.js
+++ server.js
@@ -6,20 +6,21 @@ const cors = require('cors');
 const speciesRoutes = require('./routes/speciesRoutes');
 const rangeRoutes = require('./routes/rangeRoutes');
 const observationRoutes = require('./routes/observationRoutes');
+const rangeDataRoutes = require('./routes/rangeDataRoutes');
+const speciesRangeRoutes = require('./routes/speciesRangeRoutes');
 const observationDownloadRoutes = require('./routes/observationDownloadRoutes');
 const rangeDownloadRoutes = require('./routes/rangeDownloadRoutes');
 const traitDownloadRoutes = require('./routes/traitDownloadRoutes');
 const errorHandler = require('./middleware/errorHandler');
 
-const app = express(); // Initialize Express
+const app = express();
 
 const port = process.env.PORT || 3005;
 
-const allowedOrigins = ['https://biendata.org', 'http://mint-pheasant.nceas.ucsb.edu:3004'];
+const allowedOrigins = ['https://biendata.org'];
 
 app.use(cors({
     origin: function (origin, callback) {
-        // Allow requests with no origin (like mobile apps or curl requests)
         if (!origin) return callback(null, true);
         if (allowedOrigins.indexOf(origin) === -1) {
             const msg = 'The CORS policy for this site does not allow access from the specified Origin.';
@@ -37,10 +38,14 @@ app.use(express.json());
 // Routes
 app.use('/api/species', speciesRoutes);
 app.use('/api/range', rangeRoutes);
+app.use('/api/range', rangeDataRoutes);
+app.use('/api/species-ranges', speciesRangeRoutes);
 app.use('/api/observations', observationRoutes);
 app.use('/api/download', observationDownloadRoutes);
 app.use('/api/download', rangeDownloadRoutes);
 app.use('/api/download', traitDownloadRoutes);
+
+
 
 // Error handling middleware
 app.use(errorHandler);
